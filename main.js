dbf4906947c66f5c08686ca3dc1423b82780034f
Maksim Beliaev <beliaev.m.s@gmail.com>
2022-04-19 14:12:58 +0200

    update files

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
+const { autoUpdater } = require('electron-updater');
+
+setTimeout(function(){
+        autoUpdater.checkForUpdatesAndNotify();
+    }, 7000);
 
 function createWindow () {
   // Create the browser window.
